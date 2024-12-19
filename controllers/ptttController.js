36ed04fbfd6e29634c4b9da7b10633e20c7428d8
beola134 <nguyentantai612004@gmail.com>
2024-12-19 21:53:45 +0700

    fix

--- controllers/ptttController.js
+++ controllers/ptttController.js
@@ -108,7 +108,7 @@ exports.zaloPay = async (req, res) => {
       amount: amount,
       description: `Payment for order #${transID}`,
       bank_code: "",
-      callback_url: "https://c4e2-171-243-48-82.ngrok-free.app/pttt/callback",
+      callback_url: "https://wristlywatchbe-bd4bdd62f0ed.herokuapp.com/pttt/callback",
     };
 
     const data = `${config.app_id}|${paymentData.app_trans_id}|${paymentData.app_user}|${paymentData.amount}|${paymentData.app_time}|${paymentData.embed_data}|${paymentData.item}`;
